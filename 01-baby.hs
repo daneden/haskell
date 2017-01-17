390cb55d1281de4fb8167103079b8f3a8ec50bd8
Daniel Eden <dan.eden@me.com>
2017-01-16 21:08:52 -0800

    Update

--- 01-baby.hs
+++ 01-baby.hs
@@ -5,3 +5,8 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
                          then x
                          else doubleMe x
+
+boomBangs xs = [ if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x ]
+
+length' xs = sum [1 | _ <- xs]
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
